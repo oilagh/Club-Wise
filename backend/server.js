c46bc143c906b99c429c24e6dcc2bc1c76d9a0ff
oilagh <tdk737@gmail.com>
2024-12-06 16:58:26 -0500

    Created Backend and started frontend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.route.js"
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allows us to accept JSON into the body
+
+app.use("/api/products", productRoutes)
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5010")
+});
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Box, Button } from "@chakra-ui/react"
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      {/* navbar */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App
