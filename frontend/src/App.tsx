1c48c00261c6b825dbd2071cdef8a0e90a69dd00
oilagh <tdk737@gmail.com>
2024-12-07 03:35:43 -0500

    Frontend stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Box, Button } from "@chakra-ui/react"
 import { Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <Box minH={"100vh"}>
-      {/* navbar */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
-      </Routes>
+      </Routes> 
     </Box>
   );
 }
